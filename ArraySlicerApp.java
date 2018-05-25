88e572f6b5cf8db9d7bb2f74a7a0a8fc7189eb9b
attilaKincsei <akincsei@gmail.com>
2018-05-25 15:25:54 +0200

    command line user input added; some explanatory text added to displayed result

--- ArraySlicerApp.java
+++ ArraySlicerApp.java
@@ -1,18 +1,37 @@
 import util.ArraySlicer;
+import java.util.Arrays;
 
 class ArraySlicerApp {
     public static void main(String[] args) {
+		// Creating and printing the demo array
+		System.out.println("\nArray of arrays to slice:");
     	int[][] InitialArray = new int[][] {
     		{ 2, 0, 4, 1241, 12, 5, 11, 1110, -42, 424, 1, 12323 },
     		{ 1, 3, 5, 7 },
     		{ 321, 320, 32, 3, 41241, -11, -12, -13, -66, -688 }
     	};
-    	int xCoordinate = 1;
-    	int yCoordinate = 3;
-    	int interval = 5;
+    	for (int[] subArray : InitialArray) {
+	    	System.out.println(Arrays.toString(subArray));
+    	}
+		
+		// Getting user input from command line for x and y coordinates and range:
+    	int xCoordinate = Integer.parseInt(args[0]);
+    	int yCoordinate = Integer.parseInt(args[1]);
+    	int interval = Integer.parseInt(args[2]);
+    	System.out.println("\nChosen sub-array has index " + args[0]);
+    	System.out.println(
+    		"Chosen element of sub-array has index " + args[1] + " and a value of " + Integer.toString(InitialArray[xCoordinate][yCoordinate])
+    	);
+    	System.out.println("Chosen range is " + args[2]);
+    	
+    	// Creating an instance of ArraySlicer class
     	ArraySlicer MyTwoDArray = new ArraySlicer(InitialArray);
+    	// Slicing the array by invoking the nearby method of AS class:
     	MyTwoDArray.nearby(xCoordinate, yCoordinate, interval);
+    	// Printing sliced array to terminal by invoking the PrintArraySlice method of AS class:
+    	System.out.println("\nThe result of slicing the sub-array exlcuding the the chosen element: ");
     	MyTwoDArray.PrintArraySlice();
     }
 
 }
+
--- util/ArraySlicer.java
+++ util/ArraySlicer.java
@@ -3,50 +3,43 @@ package util;
 import java.util.Arrays;
 import java.lang.Math;
 
-/* Methods needed to solve the problem in java
-creating a new integer variable: int MyIntegerVariable = 10;
-https://www.tutorialspoint.com/java/java_variable_types.htm
-indexing [integer]
-slicing: copying parts of an array
-calculating length: MyArray.length
-if conditional: ???
-return statement: return SolutionArray */
-
-/* to be continued: removing element: https://www.programiz.com/java-programming/examples/concatenate-two-arrays */
-
 public class ArraySlicer {
 	private int[][] TwoDArray;
 	
-	public int[] ArraySlice;
+	private int[] ArraySlice;
 	
 	public ArraySlicer (int[][] ArrayParam) {
-		TwoDArray = ArrayParam;
+		this.TwoDArray = ArrayParam;
 	}
 	
 	public void nearby(int xCoord, int yCoord, int interval) {
-		int[] SubArray = TwoDArray[xCoord];
+		
+		int[] SubArray = this.TwoDArray[xCoord];
 		int StartIndex1 = yCoord - Math.min(interval, yCoord);
 		int EndIndex1 = yCoord;
 		int[] ArraySlice1 = Arrays.copyOfRange(SubArray, StartIndex1, EndIndex1);
+		
 		int StartIndex2 = Math.min(yCoord + 1, SubArray.length - 1);
 		int EndIndex2 = Math.min(yCoord + interval + 1, SubArray.length - 1);
 		int[] ArraySlice2 = Arrays.copyOfRange(SubArray, StartIndex2, EndIndex2);
-		ArraySlice = new int[ArraySlice1.length + ArraySlice2.length];
+
+		this.ArraySlice = new int[ArraySlice1.length + ArraySlice2.length];
+
 		int index = 0;
 		
 		for (int number : ArraySlice1) {
-			ArraySlice[index] = number;
+			this.ArraySlice[index] = number;
 			index++;
 		}
 		
 		for (int number : ArraySlice2) {
-			ArraySlice[index] = number;
+			this.ArraySlice[index] = number;
 			index++;
 		}
     }
 
 	public void PrintArraySlice() {
-		System.out.println(Arrays.toString(ArraySlice));
+		System.out.println(Arrays.toString(this.ArraySlice));
     }
 
 }
