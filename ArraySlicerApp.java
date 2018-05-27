8b77b365cb6bf6f8249d177618d4a4c8f0942d59
attilaKincsei <akincsei@gmail.com>
2018-05-28 00:09:35 +0200

    user input changed from command line arguments to interactiveuser input, and a more comprehensive exception handling is added together with some error checks

--- ArraySlicerApp.java
+++ ArraySlicerApp.java
@@ -1,43 +1,92 @@
 import util.ArraySlicer;
 import java.util.Arrays;
+import java.io.*;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.lang.ArrayIndexOutOfBoundsException;
 
 class ArraySlicerApp {
+
+	// Creating and printing the demo array
+	public int[][] initialArray = new int[][] {
+		{ 2, 0, 4, 1241, 12, 5, 11, 1110, -42, 424, 1, 12323 },
+		{ 1, 3, 5, 7 },
+		{ 321, 320, 32, 3, 41241, -11, -12, -13, -66, -688 }
+	};
+
+	
+	public int[] userInput() {
+
+		String[] inputKeys = new String[] {
+			"x coordinate",
+			"y coordinate",
+			"interval"
+		};
+
+		String[] inputMessages = new String[] {
+			"\nFor the %s, please, enter an integer between 0 and 2 inclusively: ",
+			"\nFor the %s, please, enter an integer between 0 and %d inclusively: ",
+			"\nFor the %s, please, enter an integer greater than or equal to 0: ",
+		};
+
+		int[] inputArray = new int[3];
+		for (int i = 0; i < inputKeys.length; i++) {
+			boolean invalidInteger = true;
+			do {
+				try {
+					System.out.printf(inputMessages[i], inputKeys[i], this.initialArray[inputArray[0]].length - 1);
+					Scanner inputScanner = new Scanner(System.in);
+					inputArray[i] = inputScanner.nextInt();
+					if (inputArray[i] >= 0) {
+						boolean conditionForX = (i == 0 && inputArray[0] < this.initialArray.length);
+						boolean conditionForY = (i == 1 && inputArray[1] < this.initialArray[inputArray[0]].length);
+						boolean conditionForInterval = (i == 2);
+						if (conditionForX || conditionForY || conditionForInterval) {
+							invalidInteger = false;	
+						}
+					} else {
+						System.out.println("Not a valid integer. Please, try again.");
+					}
+					System.out.printf("You entered %s for the %s.\n", inputArray[i], inputKeys[i]);
+				} catch (InputMismatchException|ArrayIndexOutOfBoundsException error) {
+					invalidInteger = true;
+					inputArray[i] = 0;
+					System.out.println(error);
+					System.out.println("Not a valid integer. Please, try again.\n");
+				}
+			} while(invalidInteger);
+		}
+	
+		return inputArray;
+	}
+
     public static void main(String[] args) {
 		// Creating and printing the demo array
-		System.out.println("\nArray of arrays to slice:");
     	int[][] initialArray = new int[][] {
     		{ 2, 0, 4, 1241, 12, 5, 11, 1110, -42, 424, 1, 12323 },
     		{ 1, 3, 5, 7 },
     		{ 321, 320, 32, 3, 41241, -11, -12, -13, -66, -688 }
     	};
+    	
+		System.out.println("\nArray of arrays to slice:");
     	for (int[] subArray : initialArray) {
 	    	System.out.println(Arrays.toString(subArray));
     	}
-		int xCoordinate = Integer.parseInt(args[0]);
-		int yCoordinate = Integer.parseInt(args[1]);
-		int interval = Integer.parseInt(args[2]);
-		// Getting user input from command line for x and y coordinates and range:
-		System.out.printf("\nThe parameters you entered are %s, %s, %s.\n", args[0], args[1], args[2]);
-		if (xCoordinate >= 0 && xCoordinate < 3) {
-			try {
-				// Creating an instance of ArraySlicer class
-				ArraySlicer myTwoDArray = new ArraySlicer(initialArray);
-				// Slicing the array by invoking the nearby method of AS class:
-				myTwoDArray.nearby(xCoordinate, yCoordinate, interval);
-				// Printing sliced array to terminal by invoking the PrintArraySlice method of AS class:
-				int chosenElement = initialArray[xCoordinate][yCoordinate];
-				System.out.printf("\nThe result of slicing the sub-array exlcuding the the chosen element (%d): ", chosenElement);
-				myTwoDArray.printArraySlice();
-			} catch (ArrayIndexOutOfBoundsException e) {
-				int highestIndex = initialArray[xCoordinate].length - 1;
-				System.out.printf(
-				"If the first parameter is %s, then the second parameter should be between 0 and %d inclusively.\n", args[0], highestIndex);
-				System.out.printf("Error message: %s", e);
-			}
-		} else {
-			System.out.println("Each parameter should be an integer.");
-			System.out.println("The first parameter should be between 0 and 2 inclusively.\n");
-		}
+		// Getting input from user for slicing
+		ArraySlicerApp myASAPP = new ArraySlicerApp();
+		int[] userInputArray = myASAPP.userInput();
+
+		int xCoordinate = userInputArray[0];
+		int yCoordinate = userInputArray[1];
+		int interval = userInputArray[2];
+
+
+		// Slicing and printing the sub-array slice:
+		ArraySlicer myTwoDArray = new ArraySlicer(initialArray);
+		myTwoDArray.nearby(xCoordinate, yCoordinate, interval);
+		int chosenElement = initialArray[xCoordinate][yCoordinate];
+		System.out.printf("\nThe result of slicing the sub-array exlcuding the chosen element (%d):\n", chosenElement);
+		myTwoDArray.printArraySlice();
     }
 }
 
